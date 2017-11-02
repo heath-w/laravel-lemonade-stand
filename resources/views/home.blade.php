056252f3328dd78c3f4e9660e55f1ceab5c13933
Heath Williams <awilli01@amgen.com>
2017-11-02 15:57:10 -0400

    Working on styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,55 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h2>My Games</h2>
+
+                    <p><a href="#">Start a new game</a></p>
+
+                    <div class="row">                        
+                        <div class="col-sm-5">
+                            <h4>Active Games</h4>
+                            <ul class="gamesList">
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 4 - Day 8, $2500.87</a></li>
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 5 - Day 2, $25.87</a></li>
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 6 - Day 1, $5.00</a></li>
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 7 - Day 1, $5.00</a></li>
+                                
+                            </ul>
+                        </div>
+
+                        <div class="col-sm-5">
+                            <h4>Ended Games</h4>
+                            <ul class="gamesList">                                
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 1 - Day 14, $11125.17</a></li>
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 2 - Day 14, $5125.87</a></li>
+                                <li> <i class="fa fa-trash-o" aria-hidden="true"></i> <a href="#">Game 3 - Day 14, $25,000.00</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>Leader Board</h2>
+
+                            <ul class="leaderBoardList">
+                                <li>Heath - $25,000</li>
+                                <li>Mario - $12,000</li>
+                                <li>Luigi - $11,999</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <br />
+                    
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <p>You are logged in!</p>
+                        </div>
+                    </div>
+
+                    
+
+                    
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <script src="https://use.fontawesome.com/14f1f2c704.js"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
