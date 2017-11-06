a4a8710fed0ef755f53dfb9c459852c0f9e7e17e
Heath Williams <awilli01@amgen.com>
2017-11-06 10:07:29 -0500

    Restructured cond seed, added env database connection

--- database/seeds/ConditionTableSeeder.php
+++ database/seeds/ConditionTableSeeder.php
@@ -14,43 +14,57 @@ class ConditionTableSeeder extends Seeder
   {
       //
   	DB::table('condition')->insert( [
-  		'name' => 'Hot and Dry', 'base_temperature' => 90, 'percent_chance' => 0.05,
+  		'name' => 'Hot and Dry', 
+      'base_temperature' => 90, 
+      'percent_chance' => 0.05,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
   		
   	DB::table('condition')->insert( [
-  		'name' => 'Hot', 'base_temperature' => 80, 'percent_chance' => 0.10,
+  		'name' => 'Hot', 
+      'base_temperature' => 80, 
+      'percent_chance' => 0.10,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
 
   	DB::table('condition')->insert( [
-  		'name' => 'Sunny', 'base_temperature' => 75, 'percent_chance' => 0.5,
+  		'name' => 'Sunny', 
+      'base_temperature' => 75, 
+      'percent_chance' => 0.5,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
 
   	DB::table('condition')->insert( [
-  		'name' => 'Windy', 'base_temperature' => 70, 'percent_chance' => 0.1,
+  		'name' => 'Windy', 
+      'base_temperature' => 70, 
+      'percent_chance' => 0.1,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
 
   	DB::table('condition')->insert( [
-  		'name' => 'Cloudy', 'base_temperature' => 65, 'percent_chance' => 0.1,
+  		'name' => 'Cloudy', 
+      'base_temperature' => 65, 
+      'percent_chance' => 0.1,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
 
   	DB::table('condition')->insert( [
-  		'name' => 'Rainy', 'base_temperature' => 70, 'percent_chance' => 0.1,
+  		'name' => 'Rainy', 
+      'base_temperature' => 70, 
+      'percent_chance' => 0.1,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
 
   	DB::table('condition')->insert( [	
-  		'name' => 'Thunderstorms', 'base_temperature' => 70, 'percent_chance' => 0.05,
+  		'name' => 'Thunderstorms', 
+      'base_temperature' => 70, 
+      'percent_chance' => 0.05,
   		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
   		'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
   	]);
